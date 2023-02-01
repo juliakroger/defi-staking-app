02dfb79c30b56515700321c93816b456dce24ac1
Julia Kroger <juliakroger@gmail.com>
2023-02-01 07:57:56 -0300

    Reward Token Contract

---
+++ src/contracts/RWD.sol
@@ -0,0 +1,63 @@
+pragma solidity ^0.5.0;
+
+contract RWD {
+    string public name = "Reward Token";
+    string public symbol = "RWD";
+    uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens + 18 decimal place
+    uint8 public decimals = 18;
+
+    // Events: are used to inform the calling application about the current state of the contract - not stored as memory - events have lower gas cost
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    constructor() public {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, unit _value) public returns (bool success) {
+        // Ensure we have balance to make the transfer.
+        require(balanceOf[msg.sender] >= _value);
+
+        // Coming in from the Tether contract (transfer the amount and sub the balance) -> Add the balance to the receiving address
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value)
+        public
+        returns (bool success)
+    {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public returns (bool success) {
+        require(balanceOf[_from] >= _value);
+        require(allowance[_from][msg.sender] >= _value);
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        allowance[msg.sender][_from] -= _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
+    }
+}
